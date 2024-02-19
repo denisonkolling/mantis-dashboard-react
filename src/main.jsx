466431133fc0cd1cdcb8ff533d9c5b98638a74b8
Denison Kolling <115804868+denisonkolling@users.noreply.github.com>
2024-02-19 12:46:53 +0000

    remove unused dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,28 @@
-import { store } from './store';
-import reportWebVitals from './reportWebVitals';
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+// scroll bar
 import 'simplebar/src/simplebar.css';
+
+// third-party
 import { Provider as ReduxProvider } from 'react-redux';
+
+// apex-chart
 import './assets/third-party/apex-chart.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ReduxProvider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ReduxProvider>
+// project import
+import App from './App';
+import { store } from './store';
+
+// ==============================|| MAIN - REACT DOM RENDER  ||============================== //
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+    <ReduxProvider store={store}>
+      <BrowserRouter basename="/">
+        <App />
+      </BrowserRouter>
+    </ReduxProvider>
 );
-reportWebVitals();
